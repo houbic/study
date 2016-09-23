bf50ffad36e66775a035528b97e812c791374a9c
chenbinghao <chenbinghao@feinno.com>
2016-09-23 12:56:56 +0800

    master commit

---
+++ study-main/src/main/java/com/bink/utils/JsonMapper.java
@@ -0,0 +1,42 @@
+package com.bink.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+/**
+ * Created by chenbinghao on 16/9/23.上午11:50
+ */
+public class JsonMapper {
+
+    public static String toJson(Object object)
+    {
+        return JSON.toJSONString(object, new SerializerFeature[] { SerializerFeature.WriteMapNullValue, SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.WriteNonStringKeyAsString });
+    }
+
+    public static String toJsonFormat(Object object)
+    {
+        return JSON.toJSONString(object, new SerializerFeature[] { SerializerFeature.WriteMapNullValue, SerializerFeature.PrettyFormat, SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.WriteNonStringKeyAsString });
+    }
+
+    public static Object toJsonObject(Object obj)
+    {
+        return JSON.toJSON(obj);
+    }
+
+//    public static <T> T fromJson(String jsonString, Class<T> clazz)
+//    {
+//        if (StringUtils.isEmpty(jsonString)) {
+//            return null;
+//        }
+//        return JSON.parseObject(jsonString, clazz);
+//    }
+//
+//    public static <T> T fromJson(String jsonString)
+//            throws Exception
+//    {
+//        return JSON.parseObject(jsonString, new TypeReference()
+//                {
+//                }
+//                , new Feature[0]);
+//    }
+
+}
